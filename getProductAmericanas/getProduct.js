a971b22b76450e47c0db9669785eef3f450a3011
GM5\tjusto <tjusto@gm5.com.br>
2019-07-01 18:54:16 -0300

    Utilizando o módulo fetch para retornar o HTML da página do produto da Americanas

--- getProductAmericanas/getProduct.js
+++ getProductAmericanas/getProduct.js
@@ -3,5 +3,13 @@ const cheerio = require('cheerio');
 
 module.exports = function (url) {
 
-    
+    fetch(url) // Utilizando o módulo fetch para retornar o HTML da página do produto da Americanas
+        .then(res => res.text())
+        .then(body => {
+            console.log(body)
+        })
+        .catch(err => {
+            console.log('Ops! Ocorreu um erro.');
+            console.error(err);
+        });
 }
