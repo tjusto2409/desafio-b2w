bf2d38e858fbad0c5413886221750a5e48b3bf18
GM5\tjusto <tjusto@gm5.com.br>
2019-07-01 18:56:18 -0300

    Recebendo o body da página e usando o módulo cheerio para resgatar os valores da página. Criado função auxiliar para converter os valores do código e preço para inteiro e float

--- getProductAmericanas/getProduct.js
+++ getProductAmericanas/getProduct.js
@@ -6,10 +6,27 @@ module.exports = function (url) {
     fetch(url) // Utilizando o módulo fetch para retornar o HTML da página do produto da Americanas
         .then(res => res.text())
         .then(body => {
-            console.log(body)
+            const $ = cheerio.load(body); // Recebendo o body da página e usando o módulo cheerio para resgatar os valores da página
+            const id = convertInt($('.TextUI-sc-1hrwx40-0.brNcBx'));
+            const breadcrumb = [];
+            const titulo = $('#product-name-default').text();
+            const vendendor = $('.seller-name-container span').text();
+            const preco = convertInt($('.main-price .sales-price')) / 100;
+            const image = $('.image-gallery img')[0].attribs.src;
+
+            $('.product-breadcrumb span').each(function(){
+                breadcrumb.push(this.children[0].data);
+            });
+
+            
         })
         .catch(err => {
             console.log('Ops! Ocorreu um erro.');
             console.error(err);
         });
+}
+
+//Função auxiliar para converter os valores do código e preço para inteiro e float
+function convertInt(valor) {
+    return parseInt(valor.text().replace(/[^\d]+/g,''));
 }
